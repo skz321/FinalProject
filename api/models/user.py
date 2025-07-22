b8c790004a9de31c80fc07f6540790e74d0c0498
Buddi <buddin1017@gmail.com>
2025-07-22 11:57:15 -0400

    Added User and MenuItem models and schemas

---
+++ api/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from ..dependencies.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, index=True, nullable=False)
+    phone_number = Column(String(20), nullable=True)
+    address = Column(String(200), nullable=True)
+
+
+
