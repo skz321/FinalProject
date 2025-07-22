905875fe221d77dc857e8d8d9faac4381e8a39f8
WesPalanca <wespalanca@gmail.com>
2025-07-22 10:25:49 -0400

    feat: add .env functionality to config

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -1,8 +1,11 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
 class conf:
     db_host = "localhost"
     db_name = "sandwich_maker_api"
     db_port = 3306
     db_user = "root"
-    db_password = "rootroot"
+    db_password = os.getenv("SQL_PASSWORD")
     app_host = "localhost"
     app_port = 8000
