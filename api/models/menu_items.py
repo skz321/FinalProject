046c027e560575e3d69b3995d79507e2872c5e7a
Buddi <buddin1017@gmail.com>
2025-07-24 12:15:57 -0400

    feat: added ratings and review models and schemas, updated relationships in user and menu item models

--- api/models/menu_items.py
+++ api/models/menu_items.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float
 from ..dependencies.database import Base
+from sqlalchemy.orm import relationship
 
 class MenuItem(Base):
     __tablename__ = "menu_items"
@@ -9,3 +10,5 @@ class MenuItem(Base):
     price = Column(Float, nullable=False)
     calories = Column(Float, nullable=True)
     category = Column(String(50), nullable=False)
+
+    reviews = relationship("Review", back_populates="menu_item")
